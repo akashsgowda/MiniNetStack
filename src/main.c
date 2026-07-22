1a1aed34acd7e82872063e13065d1c22ac9959d3
Akash S Gowda <267418137+akashsgowda@users.noreply.github.com>
2026-07-22 11:15:55 +0530

    Make IPv4 parser independent of packet offset

--- src/main.c
+++ src/main.c
@@ -28,24 +28,24 @@ void parseEthernetHeader(unsigned char packet[])
     }
 }
 
-    void parseIPv4Header(unsigned char packet[])
-{
-    int ipv4Start = 14;
 
-    unsigned char version = packet[ipv4Start] >> 4;
+void parseIPv4Header(unsigned char packet[], int start)
+{
+    unsigned char version = packet[start] >> 4;
     printf("Version:         %u\n", version);
 
-    unsigned char ihl = packet[ipv4Start] & 0x0F;
+    unsigned char ihl = packet[start] & 0x0F;
     printf("IHL:             %u\n", ihl);
 
     unsigned short totalLength =
-        (packet[ipv4Start + 2] << 8) | packet[ipv4Start + 3];
+        (packet[start + 2] << 8) | packet[start + 3];
+
     printf("Total Length:    %u bytes\n", totalLength);
 
-    unsigned char ttl = packet[ipv4Start + 8];
+    unsigned char ttl = packet[start + 8];
     printf("TTL:             %u\n", ttl);
 
-    unsigned char protocol = packet[ipv4Start + 9];
+    unsigned char protocol = packet[start + 9];
 
     printf("Protocol ID:     %u\n", protocol);
 
@@ -67,24 +67,24 @@ void parseEthernetHeader(unsigned char packet[])
     }
 
     printf("Source IP:       %u.%u.%u.%u\n",
-           packet[ipv4Start + 12],
-           packet[ipv4Start + 13],
-           packet[ipv4Start + 14],
-           packet[ipv4Start + 15]);
+           packet[start + 12],
+           packet[start + 13],
+           packet[start + 14],
+           packet[start + 15]);
 
     printf("Destination IP:  %u.%u.%u.%u\n",
-           packet[ipv4Start + 16],
-           packet[ipv4Start + 17],
-           packet[ipv4Start + 18],
-           packet[ipv4Start + 19]);
+           packet[start + 16],
+           packet[start + 17],
+           packet[start + 18],
+           packet[start + 19]);
 }
+
 
 int main(void)
 {
     unsigned char packet[] = {
-        // =========================
+
         // Ethernet Header (14 bytes)
-        // =========================
 
         // Destination MAC
         0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
@@ -92,12 +92,11 @@ int main(void)
         // Source MAC
         0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC,
 
-        // EtherType (IPv4)
+        // EtherType IPv4
         0x08, 0x00,
 
-        // ======================
+
         // IPv4 Header (20 bytes)
-        // ======================
 
         // Version + IHL
         0x45,
@@ -117,22 +116,24 @@ int main(void)
         // TTL
         0x40,
 
-        // Protocol (TCP)
+        // Protocol TCP
         0x06,
 
         // Header Checksum
         0x00, 0x00,
 
-        // Source IP (192.168.1.100)
+        // Source IP
         0xC0, 0xA8, 0x01, 0x64,
 
-        // Destination IP (8.8.8.8)
+        // Destination IP
         0x08, 0x08, 0x08, 0x08
     };
 
+
     parseEthernetHeader(packet);
 
-    parseIPv4Header(packet);
+    parseIPv4Header(packet, 14);
+
 
     return 0;
 }
